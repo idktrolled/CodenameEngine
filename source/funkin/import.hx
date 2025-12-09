f7f54a9cd7d4ef5af8753a48841c8e0541c0a983
idktrolled <camilocarlos1100@gmail.com>
2025-12-09 14:39:03 -0500

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -33,3 +33,15 @@ import flixel.group.FlxSpriteGroup.FlxTypedSpriteGroup;
 using StringTools;
 using funkin.backend.utils.CoolUtil;
 #end
+
+#if android
+import android.content.Context as AndroidContext;
+import android.widget.Toast as AndroidToast;
+import android.os.Environment as AndroidEnvironment;
+import android.Permissions as AndroidPermissions;
+import android.Settings as AndroidSettings;
+import android.Tools as AndroidTools;
+import android.os.Build.VERSION as AndroidVersion;
+import android.os.Build.VERSION_CODES as AndroidVersionCode;
+import android.os.BatteryManager as AndroidBatteryManager;
+#end
