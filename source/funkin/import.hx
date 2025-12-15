b32c7ddace23480e7779705027312a2b52d3b910
idktrolled <camilocarlos1100@gmail.com>
2025-12-15 00:16:57 -0500

    Update import.hx

--- source/funkin/import.hx
+++ source/funkin/import.hx
@@ -42,6 +42,5 @@ import extension.androidtools.Settings as AndroidSettings;
 import extension.androidtools.Tools as AndroidTools;
 import extension.androidtools.os.Build.VERSION as AndroidVersion;
 import extension.androidtools.os.Build.VERSION_CODES as AndroidVersionCode;
-import extension.androidtools.os.BatteryManager as AndroidBatteryManager;
 #end
 #end
